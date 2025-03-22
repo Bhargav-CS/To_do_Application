8623442c57db5a7dd28f7eb82a1b6e455d50b1b8
Bhargav Patki <bhargavpatki@gmail.com>
2025-03-22 23:06:10 +0530

    added styling

--- todo/src/main.jsx
+++ todo/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/global.css' // Import the global.css file
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
